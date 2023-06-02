38aee265205a39fe4367c9983490d76c91006336
FroZy1824 <132680976+FroZy1824@users.noreply.github.com>
2023-06-02 14:59:53 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,24 +4,22 @@ input.onButtonPressed(Button.A, function () {
         strip.setBrightness(50)
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Orange))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
-        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
-        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Orange))
         strip.show()
-        basic.pause(1000)
+        basic.pause(500)
         strip.clear()
+        basic.pause(500)
     }
 })
 input.onButtonPressed(Button.B, function () {
     strip.clear()
     for (let index = 0; index < 4; index++) {
         strip.setBrightness(50)
-        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Orange))
-        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Orange))
         strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Orange))
         strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Orange))
         strip.show()
-        basic.pause(1000)
+        basic.pause(500)
         strip.clear()
+        basic.pause(500)
     }
 })
 let strip: neopixel.Strip = null
